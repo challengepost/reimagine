79123c2739ff8ea10def841aadd10d1feb92b115
Juan C. Müller and Matthew Tully <tech+jcmuller+mtully@challengepost.com>
2013-04-25 18:21:44 -0400

    IT WORKS!

--- lib/reimagine.rb
+++ lib/reimagine.rb
@@ -1,8 +1,19 @@
 require "reimagine/version"
 
+def gem_path(gem)
+  Gem::Specification.find_by_name(gem).gem_dir
+end
+
+def stylesheets_path(gem)
+  File.join(gem_path(gem), 'app', 'assets', 'stylesheets')
+end
+
 module Reimagine
   class Engine < ::Rails::Engine
     initializer :assets do |config|
+      Sass.load_paths << stylesheets_path("bourbon")
+      Sass.load_paths << stylesheets_path("neat")
+
       Rails.application.config.assets.precompile += %w(
         reimagine.css
         reimagine/base.css
