62dd62728e8463c9ebc2816c471e8bcf9adc3223
Ross Kaffenberger <rosskaff@gmail.com>
2013-04-27 09:37:28 -0400

    add images to manual sass load paths

--- lib/reimagine.rb
+++ lib/reimagine.rb
@@ -5,6 +5,7 @@ module Reimagine
     require "reimagine/engine"
   else
     Sass.load_paths << File.expand_path("../app/assets/stylesheets", __FILE__)
+    Sass.load_paths << File.expand_path("../app/assets/images", __FILE__)
     Sass.load_paths << File.expand_path("../vendor/assets/stylesheets", __FILE__)
   end
 end
