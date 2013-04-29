3d01c2c82a2fc03bde9e206e26d052dd382f868f
Ross Kaffenberger <rosskaff@gmail.com>
2013-04-29 18:38:30 -0400

    add vendor/fonts and vendor/javascripts to non-rails asset paths

--- lib/reimagine.rb
+++ lib/reimagine.rb
@@ -8,5 +8,7 @@ module Reimagine
     Sass.load_paths << File.expand_path("../app/assets/stylesheets", __FILE__)
     Sass.load_paths << File.expand_path("../app/assets/images", __FILE__)
     Sass.load_paths << File.expand_path("../vendor/assets/stylesheets", __FILE__)
+    Sass.load_paths << File.expand_path("../vendor/assets/fonts", __FILE__)
+    Sass.load_paths << File.expand_path("../vendor/assets/javascripts", __FILE__)
   end
 end
