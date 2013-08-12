6bda640e839e9a77a97a012ec728edc05fe7e2d4
Stefano B <stefano.ballabeni+github@gmail.com>
2013-08-12 16:24:49 -0400

    include compass in the rails app

--- lib/reimagine.rb
+++ lib/reimagine.rb
@@ -1,6 +1,4 @@
 require 'sass-rails'
-require 'compass-rails'
-require 'zurb-foundation'
 require 'reimagine/engine'
 
 module Reimagine
--- lib/reimagine/engine.rb
+++ lib/reimagine/engine.rb
@@ -1,6 +1,8 @@
 module Reimagine
   class Engine < ::Rails::Engine
     initializer 'reimagine.register_engine' do
+      require 'sass-rails'
+      require 'compass-rails'
       require 'zurb-foundation'
     end
   end
