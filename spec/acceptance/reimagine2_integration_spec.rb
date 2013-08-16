27b0a64a0beafb6205ef8478c4aa4e187160924c
Stefano B <stefano.ballabeni+github@gmail.com>
2013-08-16 11:33:03 -0400

    one extra test

--- spec/acceptance/reimagine2_integration_spec.rb
+++ spec/acceptance/reimagine2_integration_spec.rb
@@ -7,16 +7,23 @@ feature 'Assets' do
     page.text.should include '.large-12'
   end
 
+  scenario 'it includes ie overrides' do
+    visit '/assets/reimagine2.css'
+
+    page.text.should include '.lt-ie9 .large-12'
+  end
+
   scenario 'it prioritizes the main app overrides file over the dummy one in the engine' do
     visit '/assets/reimagine2.css'
 
     page.text.should include 'color: #bada55'
   end
 
-  scenario 'it servers reimagine2.js' do
+  scenario 'it serves reimagine2.js' do
     visit '/assets/reimagine2.js'
 
     page.text.should include 'var Reimagine2;'
     page.text.should include 'Foundation.libs.forms'
   end
+
 end
