67c560d81816af5b9d9492de62ae519e54db929c
Kevin Bongart <contact@kevinbongart.net>
2014-03-13 15:13:06 -0400

    Use tooltips Foundation tooltips

--- app/assets/javascripts/reimagine2/foundation.js
+++ app/assets/javascripts/reimagine2/foundation.js
@@ -1,6 +1,7 @@
 //= require foundation/foundation
 //= require foundation/foundation.alerts
-//= require foundation/foundation.topbar
+//= require foundation/foundation.interchange
 //= require foundation/foundation.orbit
 //= require foundation/foundation.reveal
-//= require foundation/foundation.interchange
+//= require foundation/foundation.tooltips
+//= require foundation/foundation.topbar
