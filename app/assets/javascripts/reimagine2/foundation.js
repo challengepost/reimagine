0eb6f617cf73e68db19bbb4898871885936f0dfd
Kevin Bongart <contact@kevinbongart.net>
2014-03-17 15:48:40 -0400

    Fix test where order matters

--- app/assets/javascripts/reimagine2/foundation.js
+++ app/assets/javascripts/reimagine2/foundation.js
@@ -1,7 +1,9 @@
+// The order matters
+
 //= require foundation/foundation
 //= require foundation/foundation.alerts
 //= require foundation/foundation.interchange
 //= require foundation/foundation.orbit
 //= require foundation/foundation.reveal
-//= require foundation/foundation.tooltips
 //= require foundation/foundation.topbar
+//= require foundation/foundation.tooltips
