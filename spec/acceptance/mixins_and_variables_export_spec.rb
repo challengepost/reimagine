4fbb0ca63bdb86ac32523257dd7bd3a494c9068b
Stefano B <stefano.ballabeni+github@gmail.com>
2014-03-18 12:26:58 -0400

    fix specs

--- spec/acceptance/mixins_and_variables_export_spec.rb
+++ spec/acceptance/mixins_and_variables_export_spec.rb
@@ -3,7 +3,7 @@ require 'acceptance/acceptance_helper'
 feature 'reimagine2/export' do
   scenario 'exports reimagine2 variables' do
     visit '/assets/import_exports.css'
-    save_and_open_page
+
     page.text.should include 'body.colored_with_error_color { color: #ed2918; }'
   end
 end
