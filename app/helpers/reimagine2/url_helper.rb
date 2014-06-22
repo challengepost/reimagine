a41a9d840dae0a59584e5735f117f0faf9b77501
Ross Kaffenberger <rosskaff@gmail.com>
2014-06-22 17:46:53 -0400

    add global nav to reimagine

---
+++ app/helpers/reimagine2/url_helper.rb
@@ -0,0 +1,24 @@
+module Reimagine2
+  module UrlHelper
+
+    def default_reimagine_url_options
+      {
+        scheme: 'http',
+        path: '/'
+      }
+    end
+
+    def reimagine_url(opts = {})
+      URI::HTTP.build(default_reimagine_url_options.merge(opts)).to_s
+    end
+
+    def reimagine_root_url(opts = {})
+      reimagine_url(opts.merge(host: Reimagine2.configuration.root_host))
+    end
+
+    def reimagine_post_url(opts = {})
+      reimagine_url(opts.merge(host: Reimagine2.configuration.post_host))
+    end
+
+  end
+end
