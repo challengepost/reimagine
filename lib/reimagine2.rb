7bcbb1e7c0cde6fdf3a32be09bd5cf8839b074d0
Ross Kaffenberger <rosskaff@gmail.com>
2014-06-22 17:46:14 -0400

    ability to configure reimagine2 hosts

--- lib/reimagine2.rb
+++ lib/reimagine2.rb
@@ -1,1 +1,16 @@
 require 'reimagine2/engine'
+require 'reimagine2/configuration'
+
+module Reimagine2
+  extend self
+
+  def configuration
+    @configuration || configure
+  end
+
+  def configure
+    @configuration = Configuration.new
+    yield(@configuration) if block_given?
+    @configuration
+  end
+end
