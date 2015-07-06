3524edd0f9d61861c51bbcbd3985e07cc1f219cd
Stefano B <stefano.ballabeni+github@gmail.com>
2015-07-06 15:14:51 -0400

    allow users to access the global nav menu by using their keyboard

--- app/assets/javascripts/reimagine2.js
+++ app/assets/javascripts/reimagine2.js
@@ -4,5 +4,6 @@
 //= require 'reimagine2/foundation'
 //= require 'reimagine2/base'
 //= require 'reimagine2/accessible_custom_select'
+//= require reimagine2/accessible_global_nav
 //= require 'responsive-tables'
 //= require 'prism'
