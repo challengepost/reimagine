03eb4b2d946da3ce518ae5f5003ca988d0a23803
niuage <niuage@gmail.com>
2016-01-26 13:57:37 -0500

    allows the configuration of the login and register urls

--- lib/reimagine2/configuration.rb
+++ lib/reimagine2/configuration.rb
@@ -1,9 +1,18 @@
 module Reimagine2
   class Configuration
-    attr_accessor :root_host, :post_host, :help_host
+    attr_accessor :root_host,
+      :secure_root_host,
+      :post_host,
+      :help_host,
+      :new_user_session_url,
+      :new_user_registration_url
 
     def root_host
       @root_host || 'devpost.com'
+    end
+
+    def secure_root_host
+      @secure_root_host || ["secure", root_host].join(".")
     end
 
     def post_host
@@ -14,5 +23,12 @@ module Reimagine2
       @help_host || 'help.devpost.com'
     end
 
+    def new_user_session_url
+      @new_user_session_url || URI::HTTPS.build(host: secure_root_host, path: "/users/login")
+    end
+
+    def new_user_registration_url
+      @new_user_session_url || URI::HTTPS.build(host: secure_root_host, path: "/users/register")
+    end
   end
 end
