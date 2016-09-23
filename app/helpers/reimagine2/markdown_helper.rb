970525b9c04801a871a2c7bb08769c346dd15c93
cyzanfar <cghazanfar10@gmail.com>
2016-09-23 11:55:04 -0400

    nest markdown helper in reimagine2 module

--- app/helpers/reimagine2/markdown_helper.rb
+++ app/helpers/reimagine2/markdown_helper.rb
@@ -1,10 +1,12 @@
-module MarkdownHelper
-  def markdown_render(text)
-    text ||= ""
-    DevpostMarkdown.render(text).html_safe
-  end
+module Reimagine2
+  module MarkdownHelper
+    def markdown_render(text)
+      text ||= ""
+      DevpostMarkdown.render(text).html_safe
+    end
 
-  def markdown_reference_link
-    link_to "Markdown", "https://daringfireball.net/projects/markdown/", target: "_blank"
+    def markdown_reference_link
+      link_to "Markdown", "https://daringfireball.net/projects/markdown/", target: "_blank"
+    end
   end
 end
